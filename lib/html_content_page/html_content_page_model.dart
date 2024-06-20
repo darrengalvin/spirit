2e95e136782fd9e5e38e8ff1510477a9abedcf48
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-06-20 04:05:09 +0000

    Updating to latest FlutterFlow output.

--- lib/html_content_page/html_content_page_model.dart
+++ lib/html_content_page/html_content_page_model.dart
@@ -6,30 +6,6 @@ class HtmlContentPageModel extends FlutterFlowModel<HtmlContentPageWidget> {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
-  // State field(s) for activityNameEdit widget.
-  FocusNode? activityNameEditFocusNode;
-  TextEditingController? activityNameEditTextController;
-  String? Function(BuildContext, String?)?
-      activityNameEditTextControllerValidator;
-  // State field(s) for activitySummaryEdit widget.
-  FocusNode? activitySummaryEditFocusNode;
-  TextEditingController? activitySummaryEditTextController;
-  String? Function(BuildContext, String?)?
-      activitySummaryEditTextControllerValidator;
-  // State field(s) for activityAgeGroupEdit widget.
-  FocusNode? activityAgeGroupEditFocusNode1;
-  TextEditingController? activityAgeGroupEditTextController1;
-  String? Function(BuildContext, String?)?
-      activityAgeGroupEditTextController1Validator;
-  // State field(s) for activityAgeGroupEdit widget.
-  FocusNode? activityAgeGroupEditFocusNode2;
-  TextEditingController? activityAgeGroupEditTextController2;
-  String? Function(BuildContext, String?)?
-      activityAgeGroupEditTextController2Validator;
-  // State field(s) for itemaddd widget.
-  FocusNode? itemadddFocusNode;
-  TextEditingController? itemadddTextController;
-  String? Function(BuildContext, String?)? itemadddTextControllerValidator;
 
   @override
   void initState(BuildContext context) {}
@@ -37,19 +13,5 @@ class HtmlContentPageModel extends FlutterFlowModel<HtmlContentPageWidget> {
   @override
   void dispose() {
     unfocusNode.dispose();
-    activityNameEditFocusNode?.dispose();
-    activityNameEditTextController?.dispose();
-
-    activitySummaryEditFocusNode?.dispose();
-    activitySummaryEditTextController?.dispose();
-
-    activityAgeGroupEditFocusNode1?.dispose();
-    activityAgeGroupEditTextController1?.dispose();
-
-    activityAgeGroupEditFocusNode2?.dispose();
-    activityAgeGroupEditTextController2?.dispose();
-
-    itemadddFocusNode?.dispose();
-    itemadddTextController?.dispose();
   }
 }
